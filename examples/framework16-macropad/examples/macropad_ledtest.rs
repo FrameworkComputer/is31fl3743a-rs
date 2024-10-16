a5596d4279f2ad5200e3d35eb5562b1a88575ba3
Daniel Schaefer <dhs@frame.work>
2024-10-16 22:17:19 +0800

    WIP: Correctly map LED matrix
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- examples/framework16-macropad/examples/macropad_ledtest.rs
+++ examples/framework16-macropad/examples/macropad_ledtest.rs
@@ -11,16 +11,10 @@
 use embedded_hal::digital::v2::{InputPin, OutputPin};
 use panic_halt as _;
 
-/// Maximum brightness out of 255
-///
-/// 100/255 results in 250mA current draw and is plenty bright.
-///  50/255 results in 160mA current draw and is plenty bright.
-const MAX_BRIGHTNESS: u8 = 50;
-
 // Provide an alias for our BSP so we can switch targets quickly.
 // Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
 use bsp::entry;
-use is31fl3743a::devices::UnknownDevice;
+use is31fl3743a::devices::Framework16Macropad;
 use rp_pico as bsp;
 
 use bsp::hal::{
@@ -67,7 +61,6 @@ fn main() -> ! {
         pac.I2C1,
         pins.gpio26.into_mode::<gpio::FunctionI2C>(),
         pins.gpio27.into_mode::<gpio::FunctionI2C>(),
-        // 1000,
         1000.kHz(),
         &mut pac.RESETS,
         &clocks.peripheral_clock,
@@ -77,32 +70,109 @@ fn main() -> ! {
 
     // TODO: RGB Keyboard has two, one at 0x20 and one at 0x23
     // TODO: Macroapd has just a single one at 0x20
-    let mut matrix = UnknownDevice::configure(i2c);
-    matrix
-        .setup(&mut delay)
-        .expect("failed to setup RGB controller");
-
-    matrix
-        .set_scaling(MAX_BRIGHTNESS)
-        .expect("failed to set scaling");
+    let mut matrix = Framework16Macropad::configure(i2c, 0x20);
+    matrix.setup(&mut delay).expect("failed to setup RGB controller");
+    matrix.set_scaling(0xFF).expect("failed to set scaling");
 
     loop {
+        matrix.device.pixel(0, 0, 0xFF).expect("couldn't turn on");
+        matrix.device.pixel(0, 1, 0xFF).expect("couldn't turn on");
+        matrix.device.pixel(0, 2, 0xFF).expect("couldn't turn on");
+
+        matrix.device.pixel(1, 0, 0xFF).expect("couldn't turn on");
+        matrix.device.pixel(1, 1, 0xFF).expect("couldn't turn on");
+        matrix.device.pixel(1, 2, 0xFF).expect("couldn't turn on");
+
+        matrix.device.pixel(2, 0, 0xFF).expect("couldn't turn on");
+        matrix.device.pixel(2, 1, 0xFF).expect("couldn't turn on");
+        matrix.device.pixel(2, 2, 0xFF).expect("couldn't turn on");
+
+        matrix.device.pixel(3, 0, 0xFF).expect("couldn't turn on");
+        matrix.device.pixel(3, 1, 0xFF).expect("couldn't turn on");
+        matrix.device.pixel(3, 2, 0xFF).expect("couldn't turn on");
+
+        matrix.device.pixel(20, 0, 0xFF).expect("couldn't turn on");
+        delay.delay_ms(1000);
+        matrix.device.pixel(20, 1, 0xFF).expect("couldn't turn on");
+        delay.delay_ms(1000);
+        matrix.device.pixel(16, 2, 0xFF).expect("couldn't turn on");
+        delay.delay_ms(1000);
+        matrix.device.fill(0x00).expect("couldn't turn off");
+        delay.delay_ms(1000);
+
+        matrix.device.pixel(21, 0, 0xFF).expect("couldn't turn on");
+        delay.delay_ms(1000);
+        matrix.device.pixel(21, 1, 0xFF).expect("couldn't turn on");
+        delay.delay_ms(1000);
+        matrix.device.pixel(21, 2, 0xFF).expect("couldn't turn on");
+        delay.delay_ms(1000);
+        matrix.device.fill(0x00).expect("couldn't turn off");
+        delay.delay_ms(1000);
+
+        matrix.device.pixel(22, 0, 0xFF).expect("couldn't turn on");
+        delay.delay_ms(1000);
+        matrix.device.pixel(22, 1, 0xFF).expect("couldn't turn on");
+        delay.delay_ms(1000);
+        matrix.device.pixel(22, 2, 0xFF).expect("couldn't turn on");
+        delay.delay_ms(1000);
+        matrix.device.fill(0x00).expect("couldn't turn off");
+        delay.delay_ms(1000);
+
+        matrix.device.pixel(23, 0, 0xFF).expect("couldn't turn on");
+        delay.delay_ms(1000);
+        matrix.device.pixel(23, 1, 0xFF).expect("couldn't turn on");
+        delay.delay_ms(1000);
+        matrix.device.pixel(23, 2, 0xFF).expect("couldn't turn on");
+        delay.delay_ms(1000);
+        matrix.device.fill(0x00).expect("couldn't turn off");
+        delay.delay_ms(1000);
+
+        continue;
+
         // Light up each LED, one by one
-        for y in 0..matrix.device.height {
-            for x in 0..matrix.device.width {
+        //for y in 0..matrix.device.height {
+        for y in 0..6 {
+            //for x in 0..matrix.device.width {
+            for x in 0..4 {
                 // Light up LED if system is not asleep
                 if sleep.is_low().unwrap(){
                     matrix
                         .device
                         .pixel(x, y, 0xFF)
                         .expect("couldn't turn on");
+                    delay.delay_ms(1000);
+                    matrix
+                        .device
+                        .pixel(x, y+1, 0xFF)
+                        .expect("couldn't turn on");
+                    delay.delay_ms(1000);
+                    matrix
+                        .device
+                        .pixel(x, y+2, 0xFF)
+                        .expect("couldn't turn on");
+                    delay.delay_ms(1000);
+                    //matrix
+                    //    .pixel_rgb(x, y, 0xFF, 0x00, 0x00)
+                    //    .expect("couldn't turn on");
+                    // delay.delay_ms(1000);
+                    //matrix
+                    //    .pixel_rgb(x, y, 0x00, 0xFF, 0x00)
+                    //    .expect("couldn't turn on");
+                    // delay.delay_ms(1000);
+                    //matrix
+                    //    .pixel_rgb(x, y, 0x00, 0x00, 0xFF)
+                    //    .expect("couldn't turn on");
+                    // delay.delay_ms(1000);
                 } else {
                     // Turn all LEDs off
                     matrix.device.fill(0x00).expect("couldn't turn off");
                 }
 
-                delay.delay_ms(100);
-                matrix.device.pixel(x, y, 0).expect("couldn't turn off");
+                delay.delay_ms(1000);
+                matrix.device.fill(0x00).expect("couldn't turn off");
+                // matrix
+                //     .pixel_rgb(x, y, 0x00, 0x00, 0x00)
+                //     .expect("couldn't turn off");
             }
         }
     }
